5fb87ca5d24711f44eeb75ba9fa56a9a2765cd48
Vastly <caseytraina@gmail.com>
2023-10-09 19:17:30 -0700

    Fixed some lockscreen bugs

--- Vastly/VideoObserver.swift
+++ Vastly/VideoObserver.swift
@@ -38,6 +38,7 @@ class VideoPlayerManager: ObservableObject {
     // the channel_videos is for ease-of-use.
     
     init(videos: [Channel: [Video]]) {
+        print("INIT: Video Player Manager")
         self.videos = videos
 //        updatePlayers(videos: videos)
         queue = videos[activeChannel]
@@ -129,29 +130,30 @@ class VideoPlayerManager: ObservableObject {
         commandCenter.playCommand.removeTarget(nil)
         commandCenter.pauseCommand.removeTarget(nil)
 
-//        commandCenter.nextTrackCommand.removeTarget(nil)
-//        commandCenter.previousTrackCommand.removeTarget(nil)
+        commandCenter.nextTrackCommand.removeTarget(nil)
+        commandCenter.previousTrackCommand.removeTarget(nil)
         
         commandCenter.changePlaybackPositionCommand.isEnabled = true
         commandCenter.playCommand.isEnabled = true
         commandCenter.pauseCommand.isEnabled = true
         commandCenter.skipForwardCommand.isEnabled = true
         commandCenter.skipBackwardCommand.isEnabled = true
-//        commandCenter.nextTrackCommand.isEnabled = true
-//        commandCenter.previousTrackCommand.isEnabled = true
+        commandCenter.nextTrackCommand.isEnabled = true
+        commandCenter.previousTrackCommand.isEnabled = true
 
         // Set up command center targets
         commandCenter.playCommand.addTarget { [weak self] _ in
             self?.playCurrentVideo()
             self?.pauseAllOthers(except: self?.getCurrentVideo() ?? EMPTY_VIDEO)
             self?.updateNowPlayingInfo(for: self?.getCurrentVideo() ?? EMPTY_VIDEO)
+            print("Successful Lockscreen action: Play")
             return .success
         }
 
         commandCenter.pauseCommand.addTarget { [weak self] _ in
             self?.pauseCurrentVideo()
             self?.updateNowPlayingInfo(for: self?.getCurrentVideo() ?? EMPTY_VIDEO)
-            
+            print("Successful Lockscreen action: Pause")
             return .success
         }
 
@@ -160,8 +162,10 @@ class VideoPlayerManager: ObservableObject {
                 let currentTime = player.currentTime().seconds
                 player.seek(to: CMTime(seconds: currentTime + 15, preferredTimescale: 1)) // skip forward by 15 seconds
                 self?.updateNowPlayingInfo(for: self?.getCurrentVideo() ?? EMPTY_VIDEO)
+                print("Successful Lockscreen action: Skip 15 Forward")
                 return .success
             }
+            print("Unsuccessful Lockscreen action: Skip 15 Forward")
             return .commandFailed
         }
 
@@ -170,8 +174,10 @@ class VideoPlayerManager: ObservableObject {
                 let currentTime = player.currentTime().seconds
                 player.seek(to: CMTime(seconds: max(currentTime - 15, 0), preferredTimescale: 1)) // skip backward by 15 seconds, but don't go past the beginning of the track
                 self?.updateNowPlayingInfo(for: self?.getCurrentVideo() ?? EMPTY_VIDEO)
+                print("Successful Lockscreen action: Skip 15 Back")
                 return .success
             }
+            print("Unsuccessful Lockscreen action: Skip 15 Back")
             return .commandFailed
         }
 
@@ -180,37 +186,47 @@ class VideoPlayerManager: ObservableObject {
                let event = event as? MPChangePlaybackPositionCommandEvent {
                 player.seek(to: CMTime(seconds: event.positionTime, preferredTimescale: 1))
                 self?.updateNowPlayingInfo(for: self?.getCurrentVideo() ?? EMPTY_VIDEO)
+                print("Successful Lockscreen action: Scrub")
                 return .success
             }
+            print("Unsuccessful Lockscreen action: Scrub")
             return .commandFailed
         }
         
+        commandCenter.nextTrackCommand.addTarget { [weak self] (event) -> MPRemoteCommandHandlerStatus in
+            self?.nextVideo()
+            self?.updateStaticInfo(for: self?.getCurrentVideo() ?? EMPTY_VIDEO)
+            self?.updateNowPlayingInfo(for: self?.getCurrentVideo() ?? EMPTY_VIDEO)
+            print("Successful Lockscreen action: Next")
+            return .success
+        }
+        
+        commandCenter.previousTrackCommand.addTarget { [weak self] (event) -> MPRemoteCommandHandlerStatus in
+            self?.previousVideo()
+            self?.updateStaticInfo(for: self?.getCurrentVideo() ?? EMPTY_VIDEO)
+            self?.updateNowPlayingInfo(for: self?.getCurrentVideo() ?? EMPTY_VIDEO)
+            print("Successful Lockscreen action: Previous")
+            return .success
+        }
 
         self.commandCenter = commandCenter
+        if let video = self.getCurrentVideo() {
+            self.updateStaticInfo(for: video)
+            self.updateNowPlayingInfo(for: video)
+        }
     }
     
     func updateQueue(with videos: [Video]) {
         self.queue = videos
     }
 
-    // this function updates the command center metadata that is displayed. It must be called any time a change is made to the video or its state.
-    func updateNowPlayingInfo(for video: Video) {
-        print("UPDATING INFO")
-
-//        DispatchQueue.global(qos: .userInteractive).async {
-        
-        var nowPlayingInfo = [String: Any]()
-        nowPlayingInfo[MPMediaItemPropertyTitle] = video.title
-        
-        let player = self.getPlayer(for: video)
-        print("running")
-        nowPlayingInfo = [
-            MPMediaItemPropertyTitle : video.title,
-            MPMediaItemPropertyArtist : video.author.name,
-            MPMediaItemPropertyAlbumTitle : "Vastly",
-            MPMediaItemPropertyPlaybackDuration : NSNumber(value: player.currentItem?.duration.seconds ?? 0.0),
-            MPNowPlayingInfoPropertyElapsedPlaybackTime: NSNumber(value: player.currentTime().seconds),
-            MPNowPlayingInfoPropertyPlaybackRate : player.rate] as [String : Any]
+    // Function to update static metadata
+    func updateStaticInfo(for video: Video) {
+        var staticInfo = [String: Any]()
+        staticInfo[MPMediaItemPropertyTitle] = video.title
+        staticInfo[MPMediaItemPropertyArtist] = video.author.name
+        staticInfo[MPMediaItemPropertyAlbumTitle] = "Vastly"
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = staticInfo
 
         URLSession.shared.dataTask(with: video.author.fileName ?? EMPTY_AUTHOR.fileName!) { (data, response, error) in
             guard let data = data, error == nil else {
@@ -219,13 +235,40 @@ class VideoPlayerManager: ObservableObject {
             }
             
             if let image = UIImage(data: data) {
-                nowPlayingInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(boundsSize: image.size) { _ in image }
+                staticInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(boundsSize: image.size) { _ in image }
+                MPNowPlayingInfoCenter.default().nowPlayingInfo = staticInfo
                 
                 // Update MPNowPlayingInfoCenter
-                MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+                print("Successfully updated Metadata")
             }
         }.resume()
-        
+    }
+    
+    
+    
+    // this function updates the command center metadata that is displayed. It must be called any time a change is made to the video or its state.
+    func updateNowPlayingInfo(for video: Video) {
+        print("UPDATING INFO")
+        let player = self.getPlayer(for: video)
+        let dynamicInfo: [String: Any] = [
+            MPMediaItemPropertyPlaybackDuration : NSNumber(value: player.currentItem?.duration.seconds ?? 0.0),
+            MPNowPlayingInfoPropertyElapsedPlaybackTime: NSNumber(value: player.currentTime().seconds),
+            MPNowPlayingInfoPropertyPlaybackRate : player.rate
+        ]
+
+        // This assumes that the nowPlayingInfo has been previously set, so we're merging with existing data.
+        if var existingInfo = MPNowPlayingInfoCenter.default().nowPlayingInfo {
+            for (key, value) in dynamicInfo {
+                existingInfo[key] = value
+                print("Metadata: Updated values")
+            }
+            MPNowPlayingInfoCenter.default().nowPlayingInfo = existingInfo
+            print("Metadata: Keeping values")
+
+        } else {
+            MPNowPlayingInfoCenter.default().nowPlayingInfo = dynamicInfo
+            print("Metadata: Reseting values")
+        }
     }
 
     // This function plays the next video in the currently active channel.
@@ -251,6 +294,7 @@ class VideoPlayerManager: ObservableObject {
         if index >= 0 && index < queue?.count ?? 0 {
             pauseCurrentVideo()
             current_index = index
+            self.updateStaticInfo(for: self.getCurrentVideo() ?? EMPTY_VIDEO)
             if shouldPlay {
                 playCurrentVideo()
             }
@@ -263,6 +307,7 @@ class VideoPlayerManager: ObservableObject {
 //        channel_index = Channel.allCases.firstIndex(of: channel) ?? 0
         current_index = newIndex
         activeChannel = channel
+        self.updateStaticInfo(for: self.getCurrentVideo() ?? EMPTY_VIDEO)
         if shouldPlay {
             playCurrentVideo()
         }

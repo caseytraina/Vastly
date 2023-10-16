36f05ea898398656e7e246b34743481433e451f0
Vastly <caseytraina@gmail.com>
2023-10-15 18:27:05 -0700

    Annie UI V1

---
+++ Vastly/NavBar.swift
@@ -0,0 +1,18 @@
+//
+//  NavBar.swift
+//  Vastly
+//
+//  Created by Casey Traina on 10/13/23.
+//
+
+import SwiftUI
+
+struct NavBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavBar()
+}

567ba4949f4c47955ee64185801ec00e8a5e8cb8
Michael Murray <mdmurray@gmail.com>
2023-11-21 21:35:34 +0100

    Formatting

--- Vastly/LikesListView.swift
+++ Vastly/LikesListView.swift
@@ -10,19 +10,19 @@ import SwiftUI
 struct LikesListView: View {
     @EnvironmentObject var authModel: AuthViewModel
     @EnvironmentObject var viewModel: CatalogViewModel
-    
+
     @Binding var playing: Bool
-    
+
     var body: some View {
         VideoListView(title: "Bookmarks",
-                            videos: $authModel.likedVideos,
-                            playing: $playing)
-        .environmentObject(viewModel)
-        .environmentObject(authModel)
-        .onAppear {
-            Task {
-                await authModel.fetchLikedVideos(authors: viewModel.authors)
+                      videos: $authModel.likedVideos,
+                      playing: $playing)
+            .environmentObject(viewModel)
+            .environmentObject(authModel)
+            .onAppear {
+                Task {
+                    await authModel.fetchLikedVideos(authors: viewModel.authors)
+                }
             }
-        }
     }
 }
